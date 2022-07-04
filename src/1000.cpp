33c9dbbb8a8393bcd5c042da06df6354def17380
mogrito <jwb1796@gmail.com>
2022-07-04 21:52:40 +0900

    백준 : 1000

---
+++ src/1000.cpp
@@ -0,0 +1,11 @@
+#include<iostream> 
+#include<bits/stdc++.h> //
+//문제 A와 B를 입력 받아 A+B를 출력 하시오.
+
+using namespace std;
+int a,b;
+int main(){
+    cin >> a >> b;
+    cout << a+b;
+    return 0;
+}
