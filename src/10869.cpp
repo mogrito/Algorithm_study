20d9575e88a09a0b0015edaac4f2cf68def7472e
mogrito <jwb1796@gmail.com>
2022-07-08 20:10:26 +0900

    백준 10869 , 사칙연산 , endl 줄바꿈

---
+++ src/10869.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(){
+ int a,b ;
+  cin >> a >> b; // 사칙연산
+  cout << a+b << endl ;
+  cout << a-b << endl ;
+  cout << a*b << endl ;
+  cout << a/b << endl ;
+  cout << a%b << endl ;
+  return 0; 
+} 
