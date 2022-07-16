0840c392a30cdfeda0f498923de082f8b857614b
mogrito <jwb1796@gmail.com>
2022-07-16 22:28:02 +0900

    백준 10953 / char 형

---
+++ src/10953.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+  int a,b,t;
+  char c;
+  cin >> t ;
+  for(int i=0; i<t; i++){
+    cin >> a >> c >> b ;
+    cout << a+b << endl;
+  }
+return 0;
+}
