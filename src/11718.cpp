15588564bb9b98a6d8d351763645666f1594e16a
mogrito <jwb1796@gmail.com>
2022-08-03 20:19:57 +0900

    백준11719

--- src/11718.cpp
+++ src/11718.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
   string s;
   while (s!=" "){
-    getline(cin,s); // 한줄씩 읽어들이는 getline 함수
+    getline(cin,s); // cin은 개행문자를 구분하기떄문에 한줄씩 읽어들이는 getline 함수
     cout<< s <<endl;
     if(s == "")
       break;
